12fea5a8387a6ffec53c740c05300446bfdee134
testing <jd.runza@uniandes.edu.co>
2017-08-27 23:23:24 -0500

    Taller 3

--- Webdriver/wdio.conf.js
+++ Webdriver/wdio.conf.js
@@ -38,13 +38,15 @@ exports.config = {
     // Sauce Labs platform configurator - a great tool to configure your capabilities:
     // https://docs.saucelabs.com/reference/platforms-configurator
     //
-    capabilities: [{
+capabilities: [{
         // maxInstances can get overwritten per capability. So if you have an in-house Selenium
         // grid with only 5 firefox instances available you can make sure that not more than
         // 5 instances get started at a time.
         maxInstances: 5,
-        //
-        browserName: 'firefox'
+        browserName: 'chrome',
+        chromeOptions: {
+            args: [ "--headless", "--disable-gpu", "--window-size=800,600" ]
+        }
     }],
     //
     // ===================
